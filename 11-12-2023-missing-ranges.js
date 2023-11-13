66c38a503bc43f5e591126e7f2a8ab4169002fff
NathanBaldwin <nathanrobertbaldwin@gmail.com>
2023-11-13 09:04:37 -0600

    Working easies.

---
+++ 11-12-2023-missing-ranges.js
@@ -0,0 +1,38 @@
+// You are given an inclusive range [lower, upper] and a sorted unique integer array nums, where all elements are within the inclusive range.
+
+// A number x is considered missing if x is in the range [lower, upper] and x is not in nums.
+
+// Return the shortest sorted list of ranges that exactly covers all the missing numbers. That is, no element of nums is included in any of the ranges, and each missing number is covered by one of the ranges.
+
+// Example 1:
+
+// Input: nums = [0,1,3,50,75], lower = 0, upper = 99
+// Output: [[2,2],[4,49],[51,74],[76,99]]
+// Explanation: The ranges are:
+// [2,2]
+// [4,49]
+// [51,74]
+// [76,99]
+// Example 2:
+
+// Input: nums = [-1], lower = -1, upper = -1
+// Output: []
+// Explanation: There are no missing ranges since there are no missing numbers.
+
+function findMissingRanges(nums, lower, upper) {
+  let res = [];
+
+  if (lower < nums[0]) res.push([lower, nums[0] - 1]);
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] - nums[i - 1] > 1) res.push([nums[i - 1] + 1, nums[i] - 1]);
+  }
+
+  if (nums[nums.length - 1] < upper) res.push([nums[nums.length - 1] + 1, upper]);
+
+  return res;
+}
+
+// nums = [0,1,3,50,75], lower = 0, upper = 99
+
+console.log(findMissingRanges([0, 1, 3, 50, 75], 0, 99));
